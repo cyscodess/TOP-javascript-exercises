a3757564b4d05ae40bf8bd6e4ab7d9f4bcfc4b0d
Cy <aryssesanvicente@yahoo.com>
2022-12-20 22:30:29 +0800

    Commit solutions for problems 1-5

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,5 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    return (num >= 0) ? str.repeat(num) : "ERROR"
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+    let num1 = [n1, n2].sort()[0]
+    let num2 = [n1, n2].sort()[1]
+	let arr = []
+	if ((!Number.isInteger(n1) || !Number.isInteger(n2)) || (n1 < 0 || n2 < 0)) return "ERROR"
+    
+    for (let i = num1; i <= num2; i++) {
+        arr.push(num1++)
+    }
 
-};
+    return arr.reduce((acc,curr) => acc + curr, 0)
+}
 
 // Do not edit below this line
 module.exports = sumAll;
